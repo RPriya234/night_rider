04c653b49a45bd9f517ef10db4142d753e7c0c79
RPriya234 <87373559+RPriya234@users.noreply.github.com>
2023-06-28 22:33:11 +0530

    commit 3

---
+++ Assets/scripts/ObjMovement.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjMovement : MonoBehaviour
+{
+    private Animator anim, helmetAnim;
+
+    public GameObject frontWheel, backWheel;
+    //public GameObject road, building;
+    public GameObject headlight;
+    public GameObject bike,bike_rider , helmet;
+    bool wheelrotation = false;
+    int rotationTime = 1600;
+    int Speed = 5;
+    int count;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = bike_rider.gameObject.GetComponent<Animator>();
+        helmetAnim = helmet.gameObject.GetComponent<Animator>();
+        count = 0;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)){
+            wheelrotation = !wheelrotation;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            headlight.gameObject.GetComponent<SpriteRenderer>().enabled = !headlight.gameObject.GetComponent<SpriteRenderer>().enabled;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (count == 0)
+            {
+                anim.SetBool("getOffTheBike", true);
+                anim.SetBool("hairFloatingAnimation", false);
+                count = 1;
+            }
+            else
+            {
+                anim.SetBool("getOffTheBike", false);
+                anim.SetBool("hairFloatingAnimation", true);
+                count = 0;
+            }
+        }
+
+
+        if (wheelrotation)
+        {
+            frontWheel.transform.Rotate(Vector3.forward * (rotationTime * Time.deltaTime));
+            backWheel.transform.Rotate(Vector3.forward * (rotationTime * Time.deltaTime));
+            bike.transform.Translate(Vector3.left * Speed * 3 * Time.deltaTime);
+            //building.transform.Translate(Vector3.right * Speed * Time.deltaTime);
+            //road.transform.Translate(Vector3.right * Speed*5 * Time.deltaTime);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "helmet collider")
+        {
+            helmetAnim.SetBool("helmetMove", true);
+        }
+    }
+}
